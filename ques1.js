18c666347ddba20c0d91894d042056bb6fa00157
vanessasara <127692218+vanessasara@users.noreply.github.com>
2023-11-18 22:42:30 +0500

    Add files via upload

---
+++ ques1.js
@@ -0,0 +1,2 @@
+var Name = 'john';
+console.log(" Hello ".concat(Name, " would you like to learn some typescript today?"));
---
+++ ques1.ts
@@ -0,0 +1,2 @@
+let Name = 'john' ;
+console.log( ` Hello ${Name} would you like to learn some typescript today?`);
